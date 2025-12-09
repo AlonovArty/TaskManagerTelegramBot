6821d5f889596d4b5abfa3394086e08d0a91619b
officialarcode <officialarcodemusic06@gmail.com>
2025-12-09 13:36:33 +0500

    Разработанные методы получения обновлений и ошибок. Прохоров

--- TaskManagerTelegramBot_Прохоров/Worker.cs
+++ TaskManagerTelegramBot_Прохоров/Worker.cs
@@ -1,5 +1,6 @@
 using TaskManagerTelegramBot_Прохоров.Classes;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -162,6 +163,28 @@ namespace TaskManagerTelegramBot_Прохоров
                 User.Events.Add(new Events(Time, message.Text.Replace(Time.ToString("HH:mm dd.MM.yyyy") + "\n", "")));
 
             }
+        }
+
+        private async Task HandleUpdateAsync(
+            ITelegramBotClient client,
+            Update update,
+            CancellationToken cancellationToken)
+        {
+            if (update.Type == UpdateType.Message) GetMessages(update.Message);
+
+            else if (update.Type == UpdateType.CallbackQuery)
+            {
+                CallbackQuery query = update.CallbackQuery;
+                Users User = users.Find(x=> x.IdUser == query.Message.Chat.Id);
+                Events Event = User.Events.Find(x => x.Message == query.Data);
+                User.Events.Remove(Event);
+                SendMessage(query.Message.Chat.Id, 5);
+            }
+        }
+
+        private async Task HandleErroeAsync(ITelegramBotClient client, Exception exception,HandleErrorSource source,CancellationToken token)
+        {
+            Console.WriteLine("ОШИБКА: " + exception.Message);
         }
         
     }
