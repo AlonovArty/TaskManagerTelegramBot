9c57894046b03f0e8317f55a5fdc3ca941f6bbde
officialarcode <officialarcodemusic06@gmail.com>
2025-12-09 12:55:18 +0500

    Созданный классы для приложения. Прохоров

---
+++ TaskManagerTelegramBot_Прохоров/Classes/Users.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot_Прохоров.Classes
+{
+    public class Users
+    {
+        public long IdUser { get; set; }
+        public List<Events> Events { get; set; }
+        public Users(long idUser)
+        {
+            IdUser = idUser;
+            Events = new List<Events>();
+        }
+
+    }
+}
